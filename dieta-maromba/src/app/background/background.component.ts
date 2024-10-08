8628679a1689214108dd712b3468afe9d7add78b
emillyatem <154619633+emillyatem@users.noreply.github.com>
2024-10-08 18:50:06 -0400

    inicio refeições hoje

--- dieta-maromba/src/app/background/background.component.ts
+++ dieta-maromba/src/app/background/background.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
-  styleUrl: './background.component.css'
+  styleUrls: ['./background.component.css']
 })
 export class BackgroundComponent {
+  refeicoes: string[] = ['Café da manhã', 'Almoço', 'Janta', 'Lanches/Outros'];
 
+  constructor() {
+    
+  }
 }
